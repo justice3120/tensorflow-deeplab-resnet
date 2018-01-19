7f10f4aaa1f5cb717ee16b2f1b5b11e5b6cd8a96
Masayoshi Sakamoto <masayoshi.sakamoto@dena.com>
2018-01-19 16:59:12 +0900

    up

--- deeplab_resnet/utils.py
+++ deeplab_resnet/utils.py
@@ -5,25 +5,20 @@ import tensorflow as tf
 # colour map
 label_colours = [(0,0,0)
                 # 0=background
-                ,(128,0,0),(0,128,0),(128,128,0),(0,0,128),(128,0,128)
-                # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-                ,(0,128,128),(128,128,128),(64,0,0),(192,0,0),(64,128,0)
-                # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-                ,(192,128,0),(64,0,128),(192,0,128),(64,128,128),(192,128,128)
-                # 11=diningtable, 12=dog, 13=horse, 14=motorbike, 15=person
-                ,(0,64,0),(128,64,0),(0,192,0),(128,192,0),(0,64,128)]
-                # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+                ,(255,0,0)
+                # 1=a
+                ]
 
 def decode_labels(mask, num_images=1, num_classes=21):
     """Decode batch of segmentation masks.
-    
+
     Args:
       mask: result of inference after taking argmax.
       num_images: number of images to decode from the batch.
       num_classes: number of classes to predict (including background).
-    
+
     Returns:
-      A batch with num_images RGB images of the same size as the input. 
+      A batch with num_images RGB images of the same size as the input.
     """
     n, h, w, c = mask.shape
     assert(n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (n, num_images)
@@ -61,12 +56,12 @@ def prepare_label(input_batch, new_size, num_classes, one_hot=True):
 def inv_preprocess(imgs, num_images, img_mean):
     """Inverse preprocessing of the batch of images.
        Add the mean vector and convert from BGR to RGB.
-       
+
     Args:
       imgs: batch of input images.
       num_images: number of images to apply the inverse transformations on.
       img_mean: vector of mean colour values.
-  
+
     Returns:
       The batch of the size num_images with the same spatial dimensions as the input.
     """
--- train.py
+++ train.py
@@ -20,18 +20,18 @@ from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, inv_p
 
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
 
-BATCH_SIZE = 10
-DATA_DIRECTORY = './VOCdevkit'
-DATA_LIST_PATH = './dataset/train.txt'
+BATCH_SIZE = 2
+DATA_DIRECTORY = './dataset'
+DATA_LIST_PATH = './dataset/my.txt'
 IGNORE_LABEL = 255
 INPUT_SIZE = '321,321'
 LEARNING_RATE = 2.5e-4
 MOMENTUM = 0.9
-NUM_CLASSES = 21
+NUM_CLASSES = 6
 NUM_STEPS = 20001
 POWER = 0.9
 RANDOM_SEED = 1234
-RESTORE_FROM = './deeplab_resnet.ckpt'
+RESTORE_FROM = './deeplab_resnet_init.ckpt'
 SAVE_NUM_IMAGES = 2
 SAVE_PRED_EVERY = 1000
 SNAPSHOT_DIR = './snapshots/'
